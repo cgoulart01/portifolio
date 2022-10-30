704424b94f01a0a76ad658272ea426e2b25e18c9
caiovitor.goulart <caiovitor.goulart@gmail.com>
2022-10-30 06:56:16 -0300

    atualizações

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -3,6 +3,8 @@ import { Infos, Intro, Name } from '../../styles/styles'
 import { Container } from '../../styles/Portifolio'
 import "../../styles/portifolio.module.scss"
 import Link from 'next/link'
+import { RiWhatsappFill } from 'react-icons/ri'
+import {BsGithub, BsLinkedin} from "react-icons/bs"
 const Contacts: React.FC = () => {
     return (
 <React.Fragment>
@@ -13,10 +15,15 @@ const Contacts: React.FC = () => {
         </Name>
 
         <Intro>
-        <Link href="https://www.linkedin.com/in/cgoulart01/"  ><a className="link" target='_blank'><strong className='strong'>Linkedin:</strong>{"https://www.linkedin.com/in/cgoulart01/"}</a></Link> 
+        <Link href="https://www.linkedin.com/in/cgoulart01/"  ><a className="link" target='_blank'> <BsLinkedin/> <strong className='strong'>Linkedin:</strong>{"https://www.linkedin.com/in/cgoulart01/"}</a></Link> 
             <br/>
-            <Link href="https://www.linkedin.com/in/cgoulart01/"  ><a className="link" target='_blank'><strong className='strong'>Github:</strong>{"https://www.linkedin.com/in/cgoulart01/"}</a></Link>
+            <Link href="https://github.com/cgoulart01"  ><a className="link" target='_blank'>
+            <BsGithub/>
+                <strong className='strong'>Github:</strong>{"https://github.com/cgoulart01"}</a></Link>
             <br />
+            <Link href="https://api.whatsapp.com/send?phone=5521981558598&text=Ol%C3%A1%20Caio%2C%20vi%20seu%20portif%C3%B3lio!%20Poder%C3%ADamos%20falar%20mais%20sobre%20a%20sua%20forma%20de%20trabalho%3F%3F%3F/"  ><a className="link" target='_blank'><RiWhatsappFill /><strong className='strong'>
+                Whatsapp
+                </strong>{"Clique Aqui"}</a></Link>
         </Intro>
 </Infos>
  </React.Fragment>
