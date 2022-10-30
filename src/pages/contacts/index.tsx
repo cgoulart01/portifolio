3d1e4d8eb4ded2aae242c30862e3d62eab3ef1d6
caiovitor.goulart <caiovitor.goulart@gmail.com>
2022-10-30 07:12:22 -0300

    atualizações

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -15,14 +15,14 @@ const Contacts: React.FC = () => {
         </Name>
 
         <Intro>
-        <Link href="https://www.linkedin.com/in/cgoulart01/"  ><a className="link" target='_blank'> <BsLinkedin/> <strong className='strong'>Linkedin:</strong>{"https://www.linkedin.com/in/cgoulart01/"}</a></Link> 
+        <Link href="https://www.linkedin.com/in/cgoulart01/"  ><a className="link" target='_blank'> <BsLinkedin/> <strong className='strong2'>Linkedin:</strong>{"https://www.linkedin.com/in/cgoulart01/"}</a></Link> 
             <br/>
             <Link href="https://github.com/cgoulart01"  ><a className="link" target='_blank'>
             <BsGithub/>
-                <strong className='strong'>Github:</strong>{"https://github.com/cgoulart01"}</a></Link>
+                <strong className='strong2'>Github:</strong>{"https://github.com/cgoulart01"}</a></Link>
             <br />
             <Link href="https://api.whatsapp.com/send?phone=5521981558598&text=Ol%C3%A1%20Caio%2C%20vi%20seu%20portif%C3%B3lio!%20Poder%C3%ADamos%20falar%20mais%20sobre%20a%20sua%20forma%20de%20trabalho%3F%3F%3F/"  ><a className="link" target='_blank'>
-                <RiWhatsappFill /><strong className='strong'>
+                <RiWhatsappFill /><strong className='strong2'>
                 Whatsapp
                 </strong>{"Clique Aqui"}</a></Link>
         </Intro>
